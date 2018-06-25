b1938ed5799436953512b9a9b837f0fd4b4ea013
Demon Singur <demonsingur@gmail.com>
2018-06-25 17:58:35 +0000

    msm8998-common: build camera hal
    Change-Id: If801ef72db53d7c8f4acbdc23aae063db5e50e40

--- msm8998.mk
+++ msm8998.mk
@@ -129,7 +129,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
-    camera.device@3.2-impl
+    camera.device@3.2-impl \
+    camera.msm8998
 
 # Bluetooth
 PRODUCT_PACKAGES += \
